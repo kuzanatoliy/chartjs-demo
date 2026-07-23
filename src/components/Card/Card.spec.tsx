d8436e0ec987e357ed49e59b65fcf1349cbdcb9e
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-23 17:06:44 +0300

    test: CD-24 install & confiugre packages add card unit tests

---
+++ src/components/Card/Card.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+
+import { Card } from './Card';
+
+describe('Card', () => {
+  const renderComponent = (props: { text: string }) => {
+    return render(<Card>{props.text}</Card>);
+  };
+
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it('Should render component', () => {
+    renderComponent({ text: 'content' });
+    expect(screen.getByText('content'));
+  });
+});

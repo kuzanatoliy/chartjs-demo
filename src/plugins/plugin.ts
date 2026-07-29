d575856b8c1c58a7d5eada1b517b8fccd7f5f15a
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-07-29 16:15:57 +0300

    refactor: CD-25 add navigation strategy

--- src/plugins/plugin.ts
+++ src/plugins/plugin.ts
@@ -1,38 +1,42 @@
-import type { Chart, Plugin } from 'chart.js';
+import type { ActiveDataPoint, Chart, Plugin } from 'chart.js';
 
 const store = new Map<Chart, ChartjsKeyboardPluginEngine>();
 
-class ChartjsKeyboardPluginEngine {
-  private chart: Chart;
-  private abortController = new AbortController();
+const setChartActiveElements = (
+  chart: Chart,
+  activeElements: ActiveDataPoint[]
+) => {
+  chart.setActiveElements(activeElements);
+  chart.tooltip?.setActiveElements(activeElements, { x: 0, y: 0 });
+};
 
-  private initCanvas = () => {
-    if (!this.chart.canvas.hasAttribute('tabIndex')) {
-      this.chart.canvas.setAttribute('tabIndex', '0');
-    }
-  };
-
-  private setActiveElement = (
-    chart: Chart,
-    index: number,
-    datasetIndex: number
-  ) => {
-    const activeElements = [
-      {
-        index,
-        datasetIndex,
-      },
-    ];
-    chart.setActiveElements(activeElements);
-    chart.tooltip?.setActiveElements(activeElements, { x: 0, y: 0 });
-  };
-
+type TNavigationStrategy = {
+  refreshMeta: (chart: Chart) => void;
+  goEnd: (chart: Chart) => void;
+  goNext: (chart: Chart) => void;
+  goPrevious: (chart: Chart) => void;
+  goHome: (chart: Chart) => void;
+};
+class DataFirstNavigationStrategy {
   private datasetIds: number[] = [];
   private activeDatasetId: number = 0;
   private dataLengths: number[] = [];
   private activeDataId: number = 0;
 
-  private refreshMeta = (chart: Chart) => {
+  constructor(chart: Chart) {
+    this.refreshMeta(chart);
+  }
+
+  private setChartActiveElements = (chart: Chart) => {
+    setChartActiveElements(chart, [
+      {
+        index: this.activeDataId,
+        datasetIndex: this.datasetIds[this.activeDatasetId],
+      },
+    ]);
+  };
+
+  public refreshMeta = (chart: Chart) => {
     const meta = chart.getSortedVisibleDatasetMetas();
     this.datasetIds = meta.map((item) => item.index);
     this.activeDatasetId = 0;
@@ -40,17 +44,13 @@ class ChartjsKeyboardPluginEngine {
     this.activeDataId = 0;
   };
 
-  private goEnd = (chart: Chart) => {
+  public goEnd = (chart: Chart) => {
     this.activeDatasetId = this.datasetIds.length - 1;
     this.activeDataId = this.dataLengths[this.dataLengths.length - 1] - 1;
-    this.setActiveElement(
-      chart,
-      this.activeDataId,
-      this.datasetIds[this.activeDatasetId]
-    );
+    this.setChartActiveElements(chart);
   };
 
-  private goNext = (chart: Chart) => {
+  public goNext = (chart: Chart) => {
     this.activeDataId++;
     if (this.dataLengths[this.activeDatasetId] === this.activeDataId) {
       this.activeDataId = 0;
@@ -59,14 +59,10 @@ class ChartjsKeyboardPluginEngine {
         this.activeDatasetId = 0;
       }
     }
-    this.setActiveElement(
-      chart,
-      this.activeDataId,
-      this.datasetIds[this.activeDatasetId]
-    );
+    this.setChartActiveElements(chart);
   };
 
-  private goPrevious = (chart: Chart) => {
+  public goPrevious = (chart: Chart) => {
     this.activeDataId--;
     if (this.activeDataId < 0) {
       this.activeDatasetId--;
@@ -75,26 +71,30 @@ class ChartjsKeyboardPluginEngine {
       }
       this.activeDataId = this.dataLengths[this.activeDatasetId] - 1;
     }
-    this.setActiveElement(
-      chart,
-      this.activeDataId,
-      this.datasetIds[this.activeDatasetId]
-    );
+    this.setChartActiveElements(chart);
   };
 
-  private goHome = (chart: Chart) => {
+  public goHome = (chart: Chart) => {
     this.activeDatasetId = 0;
     this.activeDataId = 0;
-    this.setActiveElement(
-      chart,
-      this.activeDataId,
-      this.datasetIds[this.activeDatasetId]
-    );
+    this.setChartActiveElements(chart);
+  };
+}
+
+class ChartjsKeyboardPluginEngine {
+  private chart: Chart;
+  private abortController = new AbortController();
+  private strategy: TNavigationStrategy;
+
+  private initCanvas = () => {
+    if (!this.chart.canvas.hasAttribute('tabIndex')) {
+      this.chart.canvas.setAttribute('tabIndex', '0');
+    }
   };
 
   private focusHandler = () => {
-    this.refreshMeta(this.chart);
-    this.goHome(this.chart);
+    this.strategy.refreshMeta(this.chart);
+    this.strategy.goHome(this.chart);
     this.chart.update();
   };
 
@@ -110,23 +110,23 @@ class ChartjsKeyboardPluginEngine {
       case 'ArrowUp':
         event.stopPropagation();
         event.preventDefault();
-        this.goPrevious(this.chart);
+        this.strategy.goPrevious(this.chart);
         break;
       case 'ArrowRight':
       case 'ArrowDown':
         event.stopPropagation();
         event.preventDefault();
-        this.goNext(this.chart);
+        this.strategy.goNext(this.chart);
         break;
       case 'Home':
         event.stopPropagation();
         event.preventDefault();
-        this.goHome(this.chart);
+        this.strategy.goHome(this.chart);
         break;
       case 'End':
         event.stopPropagation();
         event.preventDefault();
-        this.goEnd(this.chart);
+        this.strategy.goEnd(this.chart);
         break;
     }
     this.chart.update();
@@ -134,6 +134,7 @@ class ChartjsKeyboardPluginEngine {
 
   constructor(chart: Chart) {
     this.chart = chart;
+    this.strategy = new DataFirstNavigationStrategy(chart);
 
     this.initCanvas();
 

c7a7f4264baa22bfa032c114d76a2ac291f83288
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-08-11 20:36:40 +0300

    feat: CD-25 add DataSetFirstNavigationStrategy

--- src/plugins/plugin.ts
+++ src/plugins/plugin.ts
@@ -152,6 +152,49 @@ class DataFirstNavigationStrategy extends NavigationStrategy {
   };
 }
 
+class DataSetFirstNavigationStrategy extends NavigationStrategy {
+  public goEnd = () => {
+    this.activeDatasetId = this.datasetIds.length - 1;
+    this.activeDataId = this.dataIds[this.dataIds.length - 1].length - 1;
+    this.setChartActiveElements();
+  };
+
+  public goNext = () => {
+    this.activeDatasetId++;
+    if (this.activeDatasetId === this.datasetIds.length) {
+      this.activeDatasetId = 0;
+      this.activeDataId++;
+      if (this.dataIds[this.activeDatasetId].length === this.activeDataId) {
+        this.activeDataId = 0;
+      }
+    }
+    this.setChartActiveElements();
+  };
+
+  public goNextDataSet = this.goNext;
+
+  public goPrevious = () => {
+    this.activeDatasetId--;
+    if (this.activeDatasetId < 0) {
+      this.activeDatasetId = this.datasetIds.length - 1;
+      this.activeDataId--;
+      if (this.activeDataId < 0) {
+        this.activeDataId = this.dataIds[this.activeDatasetId].length - 1;
+      }
+    }
+    this.setChartActiveElements();
+  };
+
+  public goPreviousDataSet = this.goPrevious;
+
+  public goHome = () => {
+    this.activeDatasetId = 0;
+    this.activeDataId = 0;
+    this.setChartActiveElements();
+  };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class BalanceNavigationStrategy extends NavigationStrategy {
   public goEnd = () => {
     this.activeDatasetId = this.datasetIds.length - 1;
@@ -290,7 +333,8 @@ export const chartjsKeyboardPlugin: Plugin = {
       chart,
       new ChartjsKeyboardPluginEngine(
         chart,
-        new BalanceNavigationStrategy(chart)
+        new DataSetFirstNavigationStrategy(chart)
+        //new BalanceNavigationStrategy(chart)
         //new DataFirstNavigationStrategy(chart)
       )
     );

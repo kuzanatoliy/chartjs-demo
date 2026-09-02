24646055c82a76e28ef2158d7cb30f19eb98beca
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2026-09-02 15:48:00 +0300

    feat: CD-28 handle click event

--- src/plugins/chartjs-legend-keyboard-plugin/chartjs-legend-keyboard-plugin.ts
+++ src/plugins/chartjs-legend-keyboard-plugin/chartjs-legend-keyboard-plugin.ts
@@ -1,16 +1,39 @@
 import { Chart, type Plugin } from 'chart.js';
 // import { type TChartjsLegendKeyboardPluginOptions } from './types';
 
+export const isOnesetChart = (type: string) =>
+  type === 'doughnut' || type === 'pie' || type === 'polarArea';
+
 export class ChartjsLegendKeyboardPluginEngine {
   private chart: Chart;
   private legendContainer = document.createElement('div');
   private legendOptions: HTMLDivElement[] = [];
 
+  private buildClickHandler = (index: number) => () => {
+    let isSelected;
+    if (isOnesetChart(this.chart.getSortedVisibleDatasetMetas()[0].type)) {
+      this.chart.toggleDataVisibility(index);
+      isSelected = this.chart.getDataVisibility(index);
+    } else if (this.chart.isDatasetVisible(index)) {
+      isSelected = false;
+      this.chart.hide(index);
+    } else {
+      isSelected = true;
+      this.chart.show(index);
+    }
+    this.legendOptions[index].setAttribute(
+      'aria-selected',
+      isSelected.toString()
+    );
+    this.chart.update();
+  };
+
   private init = () => {
-    this.legendContainer.style.position = 'fixed';
-    this.chart.legend?.legendItems?.map((item) => {
+    this.legendContainer.style.position = 'absolute';
+    this.chart.legend?.legendItems?.map((item, ind) => {
       const option = document.createElement('div');
       this.legendOptions.push(option);
+      option.addEventListener('click', this.buildClickHandler(ind));
       option.setAttribute('aria-label', item.text);
       option.style.position = 'absolute';
       option.style.outline = 'solid';
@@ -41,7 +64,6 @@ export class ChartjsLegendKeyboardPluginEngine {
       item.style.height = `${height}px`;
     });
     this.legendContainer.style.outline = 'solid';
-    console.log('called');
   };
 
   constructor(chart: Chart) {
@@ -61,12 +83,18 @@ const store = new Map<Chart, ChartjsLegendKeyboardPluginEngine>();
 
 export const chartjsLegendKeyboardPlugin: Plugin = {
   id: 'chartjsLegendKeyboardPlugin',
-  afterInit: (
+  /*afterInit: (
     chart: Chart
     //_,
     // options: TChartjsLegendKeyboardPluginOptions
   ) => {
     store.set(chart, new ChartjsLegendKeyboardPluginEngine(chart));
+  },*/
+
+  beforeDraw: (chart: Chart) => {
+    if (!store.get(chart)) {
+      store.set(chart, new ChartjsLegendKeyboardPluginEngine(chart));
+    }
   },
 
   beforeDestroy: (chart: Chart) => {
